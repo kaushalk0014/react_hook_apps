738b6ce4c14575fdb918621ec5acbe4603d57c7c
kaushal kumar <kaushal.k.0014@gmail.com>
2025-06-01 18:57:02 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import DisplayList from './components/DisplayList';
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DisplayList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const DisplayList = () => {
+
+    const [show, setShow] = React.useState(false);
+    const [userInput, setUserInput] = React.useState("")
+    const [listValue, setListValue] = React.useState<string[]>([]);
+    const inputRef = React.useRef<HTMLInputElement>(null)
+
+    const handelClick = () => {
+        setShow(true);
+            setListValue((prev)=>[
+                ...prev,
+                userInput.trim()
+            ]);
+             setUserInput("");
+             inputRef.current?.focus()
+        }
+    
+    const handelChange = (value:string) =>{
+            setUserInput(value);
+    }
+    return (
+        <div>
+            <input type="text" ref={inputRef} value={userInput} onChange={(e)=>handelChange(e.target.value)} placeholder="Enter text here" />
+            <button onClick={() => handelClick()}>Submit</button>
+            
+            {show && (
+                listValue.length  && listValue.map((item, index) => (
+                    <div key={index}>
+                       <li> {item}</li>
+                    </div>
+                ))
+
+            )}
+        </div>
+    )
+};
+
+export default DisplayList;
